b757a77ee7e19ffc200febdcef6b740f17a97244
Mark Hamhuis <57069136+markkram04@users.noreply.github.com>
2024-10-16 13:16:19 +0200

    #1 Initial commit
    moi

---
+++ Vinyl collection app/Vinyl_collection_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  Vinyl_collection_appApp.swift
+//  Vinyl collection app
+//
+//  Created by Mark Hamhuis on 16/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct Vinyl_collection_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
